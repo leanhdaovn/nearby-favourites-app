f77f05d40e9010708fe4118472e6b9d66e6321c7
Dao Le <leanhdaovn@gmail.com>
2017-03-18 23:15:22 +0800

    Extract googleApiKeySelector

--- src/components/PlaceImage/index.js
+++ src/components/PlaceImage/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Image } from 'react-native';
 import { connect } from 'react-redux';
-import get from 'lodash/fp/get';
+import { googleApiKeySelector } from '../common/selectors';
 
 const placePhotoUrl = (googleApiKey, place) => {
   const photo = place.photos ? place.photos[0] : null;
   const photoReference = photo ? photo.photo_reference : null;
   const url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&maxheight=160&photoreference=${photoReference}&key=${googleApiKey}`;
   return url;
-  // console.log(url);
-  // return '_';
 };
 
 const mapStateToProps = (state, {place}) => {
-  const googleApiKey = get('api.googleApiKey')(state);
   return {
     style: {width: 100, height: 80},
-    source: {uri: placePhotoUrl(googleApiKey, place)}
+    source: {uri: placePhotoUrl(googleApiKeySelector(state), place)}
   }
 }
 
